fb36c209cbd15f185bebd292a1b42de9b2dcaf84
JingShang Lu <jingshanglu1@gmail.com>
2022-03-16 12:11:23 +0800

    add test case for savepoint

---
+++ src/main/java/com/sphereex/utils/OpenGaussUtil.java
@@ -0,0 +1,32 @@
+package com.sphereex.utils;
+
+import com.sphereex.core.DBInfo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class OpenGaussUtil {
+
+    private volatile static OpenGaussUtil instance;
+
+    private OpenGaussUtil() throws ClassNotFoundException {
+        Class.forName("org.opengauss.Driver");
+    }
+
+    public static OpenGaussUtil getInstance() throws ClassNotFoundException {
+        if (null == instance) {
+            synchronized (OpenGaussUtil.class) {
+                if (null == instance) {
+                    instance = new OpenGaussUtil();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public Connection getConnnection(DBInfo dbInfo) throws SQLException {
+        Connection conn = DriverManager.getConnection(String.format("jdbc:opengauss://%s:%d/%s", dbInfo.getIp(), dbInfo.getPort(), dbInfo.getDbName()), dbInfo.getUser(), dbInfo.getPassword());
+        return conn;
+    }
+}
