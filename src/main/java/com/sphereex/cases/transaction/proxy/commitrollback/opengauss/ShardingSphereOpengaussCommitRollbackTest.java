ccea8192d49da5f006789fe29b5d1de53b7d4f57
zhangcheng <flyingzc@outlook.com>
2022-07-12 09:47:40 +0800

    Add commit and rollback test cases

---
+++ src/main/java/com/sphereex/cases/transaction/proxy/commitrollback/opengauss/ShardingSphereOpengaussCommitRollbackTest.java
@@ -0,0 +1,42 @@
+package com.sphereex.cases.transaction.proxy.commitrollback.opengauss;
+
+import com.sphereex.cases.transaction.proxy.commitrollback.ShardingCommitRollbackTest;
+import com.sphereex.core.AutoTest;
+import com.sphereex.core.CaseInfo;
+import com.sphereex.core.DBType;
+import lombok.Getter;
+
+@AutoTest
+public final class ShardingSphereOpengaussCommitRollbackTest extends ShardingCommitRollbackTest {
+
+    @Getter
+    private final String yamlFile = null;
+
+    @Override
+    public void init() {
+        String name = "ShardingSphereOpengaussCommitRollbackTest";
+        String feature = "transaction";
+        String tag = "commitRollback";
+        String message =
+                "test for rollback:\n" +
+                        "1. create a session\n" +
+                        "2. begin a transaction\n" +
+                        "3. check if row count is 0\n" +
+                        "4. insert one row (1,1,1)\n" +
+                        "5. check if row count is 1\n" +
+                        "6. rollback\n" +
+                        "7. check if row count is 0"
+
+                        + "test for commit:\n" +
+                        "1. create a session\n" +
+                        "2. begin a transaction\n" +
+                        "3. check if row count is 0\n" +
+                        "4. insert one row (1,1,1)\n" +
+                        "5. check if row count is 1\n" +
+                        "6. commit the transaction\n" +
+                        "7. check if row count is 1";
+        String configPath = "conf/case/transaction/proxy/commitrollback/opengauss";
+        String clientType = "proxy";
+        caseInfo = new CaseInfo(name, feature, tag, message, DBType.OPENGAUSS, clientType, configPath);
+    }
+}
